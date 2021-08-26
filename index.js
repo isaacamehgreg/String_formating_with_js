028d6b8e7511d89ae57c55a71f26823e06792c8a
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-08-26 23:17:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+
+app.set('view-engine', 'ejs');
+
  
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8000)
